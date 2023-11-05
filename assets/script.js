fc8593625e0a58da154e65e42bc1b84528ffe4f1
marialwj <mariaa.georgy@gmail.com>
2023-11-04 23:27:42 -0700

    adjusted code to accomodate with the requirements

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-var choiceArr =[]
+choiceArr =[]; {
 var length= 8;
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var specialcharac = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|', '~'];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
 
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -12,36 +13,34 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-function writePassword() {
-    var correctpassword = generatePrompts();
-    var passwordText = document.querySelector("#password");
-
-    if (correctpassword){
-      var generatedPassword = generatePassword();
-      passwordText.value = generatedPassword;
-    } else {
-      passwordText.value = "";
-    }
-}
-
-function generatePassword (){
+function generatePassword() {
   var password = "";
-  for(var i = 0; i < length; i++) {
-      var randomLetter = Math.floor(Math.random() * choiceArr.length);
-      password = password + choiceArr[randomLetter];
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
   }
   return password;
-
 }
 
+function writePassword() {
+  var correctpassword = generatePrompts();
+  var passwordText = document.querySelector("#password");
+
+  if (correctpassword) {
+    var generatedPassword = generatePassword();
+    passwordText.value = generatedPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
 
 function generatePrompts() {
   console.log("Hi! You clicked the button.")
   choiceArr = [];
 
-length = parseInt(prompt("How many characters would you like your password to be? (10-40 characters"));
-  if(isNaN(length) || length < 10 || length > 40 ) {
-    alert("Length has to be a NUMERICAL value between 10-40 😋 Try Again!");
+length = parseInt(prompt("How many characters would you like your password to be? (8-128 characters"));
+  if(isNaN(length) || length < 8 || length > 128 ) {
+    alert("Length has to be a NUMERICAL value between 8-128 😋 Try Again!");
     return false;
 }
   if (confirm("Hit 'OK' if you want upper case letters")) {
